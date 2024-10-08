3408e41b1095a8c02e808a377f18c809db32a598
Danila <113025911+Danila2510@users.noreply.github.com>
2024-10-08 15:59:27 +0300

    first commit

---
+++ src/data/users.js
@@ -0,0 +1,18 @@
+export const users = [
+    {
+        "id": 1,
+        "login": "Nik",
+        "password": "$2b$10$J3V5n0.mIN9C2TnSDUPvNuHf0OXyGx2LIxJ2dKcq9wFuTEZf8Qrym"
+    },
+    {
+        "login": "Kto",
+        "email": "nik@mail.com",
+        "password": "$2b$10$Se5xgLhCqzbCVROe6079mu0dFNehyI74QJxcOsguFq.Tx3BOGhl7O"
+    },
+    {
+        "login": "Danil",
+        "email": "node@mail.com",
+        "password": "$2b$10$uUPcblS2H7/L/vqe6R8n0uE.Rr8fZw8sypa5yC8k0097tw/HnASIS"
+    }
+];
+
